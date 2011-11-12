77162ed387d48786571735f83d1bb477318a3a97
Jon Lebensold <jon@lebensold.ca>
2011-11-12 21:20:01 +0200

    implemented a bootstrap template

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 require '../Slim/Slim.php';
-require '../Name.php';
 
 function json($obj)
 {
@@ -12,26 +11,15 @@ $app = new Slim();
 $app->config(array('templates.path' => '../templates'));
 
 $app->get('/names/:id',function($id) {
-  echo json(Name::find($id));
 });
 $app->get('/names',function() {
-  echo json(Name::findAll());
 });
 $app->post('/names',function() use ($app) {
-  $n = new Name(null,$app->request()->post('name'));
-  $n->create();
   echo '{status: "success" }';
 }); 
 $app->put('/names/:id',function($id) use ($app) {
-  $name = Name::find($id);
-  $name->name = $app->request()->put('name');
-  $name->update();
-
-  echo json(Name::findAll());
 });
 $app->delete('/names/:id', function($id) use ($app) {
-  Name::delete($id);
-  echo json(Name::findAll());
 });
 
 $app->get('/', function() use($app) {
--- templates/home.tpl.php
+++ templates/home.tpl.php
@@ -1,120 +1,50 @@
 <html>
   <head>  
     <title>Names Hello</title>
-<script type="text/javascript" src="/js/jquery-1.5.1.js"></script>
+      <link type="text/css" rel="stylesheet" media="screen" href="/css/bootstrap-1.4.0.min.css"></link>
+
+      <script type="text/javascript" src="/js/lib/jquery-1.7.0.min.js"></script>
 		<style>
-			* 
-			{
-
-				margin: 0 0;
-				padding: 0 0;
-				font-family: helvetica, arial, sans-serif;
-			}
-			body {
-
-				background: #CCC;
-			}
-			#container
-			{
-				padding: 20px;
-				margin: 0 auto;
-				width: 600px;
-				background: #FFF;
-      }
-
-      #names
-      {
-        width:150px;
-      }
-      .delete
-      {
-        margin-top:4px;
-        font-size: 9px;
-        float: right;
-      }
 
     </style>
-    <script>
-      $(function()
-      {
-        $("#names").delegate('.edit','click',function(e)
-          {
-            $(this).hide()
-            $(this).parent().append('<input type="text" value="'+$(this).text()+'"></input><button class="save">save</button>');
-          });
-        $("#names").delegate('a.delete','click',function(e)
-          {
-            $.ajax({type: 'DELETE',
-                    dataType: 'json',
-                    url: '/names/'+$(this).parents('li').data('id'),
-                    success: function(resp)
-                    {
-                      renderNames(resp);
-                    }});
-            e.preventDefault();
-          });
-        $("#names").delegate('button.save','click',function(e)
-          {
-            $.ajax({type: 'PUT',
-                    dataType: 'json',
-                    data : {
-                             name: $(this).parent().find('input').val()
-                    },
-                    url : '/names/'+$(this).parents('li').data('id'),
-                    success : function(resp)
-                    {
-                      renderNames(resp);
-                    }
-            });
-            e.preventDefault();
-          });
-        $('a.list').click(function(e)
-          {
-            findAll();
-            e.preventDefault();
-          });
-
-        $("form").submit(function(e)
-          {
-            $.post("/names", 
-              {name: $("#newname").val() },
-              function(resp) {
-                findAll(); 
-              });
-            e.preventDefault();
-          });
-
-      });
-
-      function findAll()
-      {
-        $.get("/names",function(resp)
-        {
-          renderNames(resp);
-        },"json");
-      };
-    function renderNames(names)
-    {
-        $("#names").empty();
-      $.each(names,function(k,v)
-      {
-        $("#names").append('<li id="name_'+v.id+'" data-id="'+v.id+'"><a class="edit">'+v.name+'</a>'+
-          '<small><a href="#" class="delete">delete</a></small></li>');
-      });
-    }
-
-    </script>
 
   </head>
   <body>
-    <div id="container">
-      <h3> Names <a class="list" href="#">Load</a></h3>  
-      <ul id="names"></ul>
-    
-      <form class="" id="newnamefrm">
-        <input type="text" id="newname"></input><input type="submit" value="Add Name" />
-      </form>  
+<div class="topbar">
+      <div class="fill">
+        <div class="container">
+          <a class="brand" href="#">Scratchpad</a>
+          <ul class="nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#help">Help</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
+
+    <div class="container">
+
+      <div class="content">
+        <div class="page-header">
+          <h1>Page name <small>Supporting text or tagline</small></h1>
+        </div>
+        <div class="row">
+          <div class="span10">
+            <h2>Main content</h2>
+          </div>
+          <div class="span4">
+            <h3>Secondary content</h3>
+          </div>
+        </div>
+      </div>
+
+      <footer>
+        <p>from @Zendcasts</p>
+      </footer>
+
+    </div> <!-- /container -->
+  
+
   </body>
 
 </html>
